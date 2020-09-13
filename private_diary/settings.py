58f966621a70abeca61708d555b2abb07ba20ffb
Nobuo Imai <nobuo.imai.ni@gmail.com>
2020-09-13 16:50:58 +0900

    Add database setting

--- private_diary/settings.py
+++ private_diary/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'private_diary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'private_diary',
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
     }
 }
 
